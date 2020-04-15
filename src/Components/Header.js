a2c50310f13d0c9d37e97ae634b02a70ff3b5df7
Ian McElroy <mcelroyian@gmail.com>
2020-04-15 15:50:11 -0500

    basic page structure

---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../App.css"
+
+function Header() {
+
+
+return (
+    <header>
+        <div>
+            <h1>Nasa Photo of the Day, Plus MORE...</h1>
+        </div>
+    </header>
+)
+    
+}
+
+export default Header
---
+++ src/Components/Main.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import "../App.css"
+
+function Main() {
+
+
+    
+return (
+    <div className='wrapper'>
+        <div>
+            <div><h2>Intro</h2></div>
+            <div><h2>Extraz</h2></div>
+        </div>
+        <div>
+            <h2>Photo of the Day</h2>
+            <img src='x' alt='cool astranomical aspect' />
+        </div>
+    </div>
+)
+    
+}
+
+export default Main
